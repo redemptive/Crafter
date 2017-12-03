5d075b5aec31a4980a0a8738401daf539d37df8c
Ewan Forbes <ewan_forbes@yahoo.co.uk>
2017-12-03 17:45:36 +0000

    Add files via upload

--- crafter.js
+++ crafter.js
@@ -1,32 +1,73 @@
-var map = []
+$(document).ready(function() {
 
-var gameArea = {
-		canvas : document.createElement("canvas"),
-		start : function () {
-			this.canvas.width = document.body.clientWidth;
-			this.canvas.height = 700;
-			this.context = this.canvas.getContext("2d");
-			document.body.insertBefore(this.canvas,document.body.childNodes[0]);
-			this.interval = setInterval(updateGameArea, 20);
-		},
-		clear : function () {
-			this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-			this.draw(this.canvas.height,this.canvas.width,0,0,"green");
-		},
-		draw : function (height,width,x,y,color,rotation = 0) {
-			//Draw function with rotation if provided
-			this.context.save();
-			this.context.fillStyle = color;
-			this.context.translate(x,y);
-			this.context.rotate(rotation);
-			this.context.fillRect(0, 0, width, height);
-			this.context.restore();
-		},
-		drawText : function (theString, x, y, size = 16) {
-			//Draw function for text
-			this.context.save();
-			this.context.font = size + "px Verdana";
-			this.context.fillText(theString, x, y);
-			this.context.restore();
+	var map = [];
+	var tileSize = 25;
+	var mapSize = 20;
+	var tiles = [
+		"black",
+		"grey",
+		"blue",
+		"white",
+	];
+
+	var gameArea = {
+			canvas : document.createElement("canvas"),
+			start : function () {
+				this.canvas.width = 700;
+				this.canvas.height = 700;
+				this.context = this.canvas.getContext("2d");
+				document.body.insertBefore(this.canvas,document.body.childNodes[0]);
+				this.interval = setInterval(updateGameArea, 20);
+			},
+			clear : function () {
+				this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+				this.draw(this.canvas.height,this.canvas.width,0,0,"white");
+			},
+			draw : function (height,width,x,y,color,rotation = 0) {
+				//Draw function with rotation if provided
+				this.context.save();
+				this.context.fillStyle = color;
+				this.context.translate(x,y);
+				this.context.rotate(rotation);
+				this.context.fillRect(0, 0, width, height);
+				this.context.restore();
+			},
+			drawText : function (theString, x, y, size = 16) {
+				//Draw function for text
+				this.context.save();
+				this.context.font = size + "px Verdana";
+				this.context.fillText(theString, x, y);
+				this.context.restore();
+			}
+		};
+		
+	function startGame() {
+		initiateMap();
+	}
+
+	function drawMap() {
+		for (var i = 0; i < map[0].length;i++) {
+			for (var j = 0; j < map[1].length;j++) {
+				gameArea.draw(tileSize,tileSize,i*tileSize,j*tileSize,tiles[map[i][j]]);
+			}
 		}
 	}
+
+	function initiateMap() {
+		for (var i = 0; i < mapSize;i++) {
+			map.push([]);
+			for (var j = 0; j < mapSize;j++) {
+				map[i].push(Math.floor(Math.random() * 3));
+			}
+		}
+	}
+
+	function updateGameArea() {
+		gameArea.clear();
+		drawMap();
+	}
+
+	gameArea.start();
+	startGame();
+
+});
